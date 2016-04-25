371c72dc4a598ae845131324bae21435eb120239
Pontus Johansson <pontusjohansson@live.com>
2016-04-25 15:23:28 +0200

    Bullshit

---
+++ SlaskEngine/sources/Loader.cpp
@@ -0,0 +1,35 @@
+#include "Loader.h"
+
+
+
+Loader::Loader()
+{
+}
+
+
+Loader::~Loader()
+{
+}
+
+/*
+[12:17:45] Slask: I think I understand how to write this maybe
+[12:18:02] Zas: good luck Kappa
+[12:18:34] Slask: inb4 I point at turn of the computer somehow
+*/
+
+
+void Loader::load(SpriteSet *set)
+{	
+	mtxloader.lock();
+	spriteset.push_back(set);
+	mtxloader.unlock();
+}
+
+/*
+void Loader::load(SoundSet *set)
+{
+	mtxloader->lock();
+	soundset.pushback(set);
+	mtxloader->unlock();
+}
+*/
